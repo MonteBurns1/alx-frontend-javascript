6161b17fcfd252c2dc1f14b6e3097c26557e4d9f
MonteBurns1 <144336632+MonteBurns1@users.noreply.github.com>
2025-04-18 15:09:38 +0200

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,10 @@
-import ClassRoom from './0-classroom.js';
+
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
+  const classroom1 = new ClassRoom(19);
+  const classroom2 = new ClassRoom(20);
+  const classroom3 = new ClassRoom(34);
+
+  return [classroom1, classroom2, classroom3];
 }
