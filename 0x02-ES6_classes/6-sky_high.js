fd76d8ef90b471c4539b5abd611f84c625d9a9c7
MonteBurns1 <144336632+MonteBurns1@users.noreply.github.com>
2025-04-18 15:22:28 +0200

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,26 +1,21 @@
+
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    if (typeof floors !== 'number') {
-      throw new TypeError('Floors must be a number');
-    }
-    this._floors = floors;
+    this.floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(newFloors) {
-    if (typeof newFloors !== 'number') {
-      throw new TypeError('Floors must be a number');
-    }
-    this._floors = newFloors;
+  set floors(value) {
+    this._floors = value;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
